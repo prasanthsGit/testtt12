e395dbe84df4bb6e479fe8d214d9ada4f911a428
prasanthsGit <prasanthsekar1106@gmail.com>
2021-03-03 20:12:41 +0530

    my next commit

--- testtt12/ViewController.swift
+++ testtt12/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test")
+        if "2" == "2" {
+            print("true")
+        } else {
+            print("false")
+        }
     }
 
 
